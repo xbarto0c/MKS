d0c26434303b7825e396ed4075016acd2960deee
Jan Barton <honza.barton1@gmail.com>
2022-10-05 16:01:56 +0200

    Cvičení 2 - podúkol 3

--- cv2/Src/main.c
+++ cv2/Src/main.c
@@ -23,8 +23,56 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+volatile uint32_t Tick = 0;
+#define LED_TIME_SHORT (uint8_t)100
+#define LED_TIME_LONG (uint16_t)1000
+#define LED_TIME_BLINK (uint16_t)300
+
+void blikac(void) // used to check if the time has already come for the led to change it's state
+{
+	static uint32_t delay;
+
+	if (Tick > (delay + LED_TIME_BLINK))
+	{
+		GPIOA->ODR ^= (1 << 4);
+		delay = Tick;
+	}
+}
+
+void tlacitka(void)
+{
+	static uint32_t old_s2;
+	static uint32_t off_time;
+	static uint32_t old_s1;
+
+	uint32_t new_s2 = GPIOC->IDR & (1 << 0);
+	uint32_t new_s1 = GPIOC->IDR & (1 << 1);
+
+	if (old_s2 && !new_s2)
+	{ // falling edge
+		off_time= Tick + LED_TIME_SHORT;
+		GPIOB->BSRR = (1 << 0);
+	}
+	old_s2 = new_s2;
+
+	if (old_s1 && !new_s1)
+	{ // falling edge
+		off_time = Tick + LED_TIME_LONG;
+		GPIOB->BSRR = (1 << 0);
+	}
+	old_s1 = new_s1;
+
+	if (Tick > off_time)
+	{
+		GPIOB->BRR = (1 << 0);
+	}
+
+}
+
 int main(void)
 {
+	SysTick_Config(8000); // 1ms
+
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
 	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
 	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
@@ -32,26 +80,34 @@ int main(void)
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
-	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+
+	/*SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
 	EXTI->IMR |= EXTI_IMR_MR0; // mask
 	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
-	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1*/
+
 
 	for(;;)
 	{
-//		GPIOA->BSRR = (1 << 4);
-//		GPIOB->BSRR = (1 << 0);
+		blikac();
+		tlacitka();
 	}
 }
 
-void EXTI0_1_IRQHandler(void)
+/*void EXTI0_1_IRQHandler(void)
 {
 	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
 		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
-		GPIOA->ODR ^= (1 << 4); // toggle - reads ODR register's value then xors it with the right side
-		GPIOB->ODR ^= (1 << 0); // toggle
+		GPIOB->ODR ^= (1 << 0); // toggle - reads ODR register's value then xors it with the right side
 	}
+}*/
+
+void SysTick_Handler(void) // code that executes once the system counter increments
+{
+	Tick++;
 }
+
+
 
 /*int main(void)
 {
