0c4eb9308dfa4573f1a818fa4bd2ff87cb57d87e
Jan Barton <honza.barton1@gmail.com>
2022-10-26 16:49:51 +0200

    EEPROM příkazy - hotovo

--- cv_5/Core/Src/main.c
+++ cv_5/Core/Src/main.c
@@ -23,6 +23,8 @@
 /* USER CODE BEGIN Includes */
 
 #include "stdio.h" // to be able to use printf
+#include "string.h" // to be able to work with strings
+#include "stdlib.h" // to be able to use the function atoi()
 
 /* USER CODE END Includes */
 
@@ -37,7 +39,8 @@
 #define RX_BUFFER_LEN 64
 #define uart_rx_write_ptr (RX_BUFFER_LEN - hdma_usart2_rx.Instance->CNDTR) // pointer pointing to the place in memory, where the last write was performed
 #define CMD_BUFFER_LEN 256 // length of the support buffer
-
+#define EEPROM_ADDR 0xA0
+#define read_num_of_bytes 32
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -46,6 +49,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_rx;
 
@@ -61,6 +66,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -70,6 +76,7 @@ static void MX_USART2_UART_Init(void);
 
 int _write(int file, char const *buf, int n) // strong function, tells the stdio library, where to print data to
 {
+	/* stdout redirection to UART2 */
 	HAL_UART_Transmit(&huart2, (uint8_t*)(buf), n, HAL_MAX_DELAY);
 	return n;
 }
@@ -105,13 +112,56 @@ static void uart_process_command (char *cmd)
 				printf("OK\n");
 			}
 	}
-	else if (strcasecmp(token, "STATUS") == 0)
+	else if (strcasecmp(token, "STATUS") == 0) // if the string says "status", return the status of both leds
 	{
 		if(HAL_GPIO_ReadPin(GPIOA, LED1_Pin)) printf("LED1 is turned on\n");
 		else printf("LED1 is turned off\n");
 		if(HAL_GPIO_ReadPin(GPIOB, LED2_Pin)) printf("LED2 is turned on\n");
 		else printf("LED2 is turned off\n");
 	}
+	else if (strcasecmp(token, "READ") == 0) // if the first parameter is READ,
+	{
+		static uint16_t addr = 0;
+		static uint8_t value = 0;
+		token = strtok(NULL, " ");
+		addr = atoi(token);
+
+		HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, addr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000); // bus name, device address, memory address, memory indexing method, where to save the read data, number of bytes to read, timeout
+		while (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_ADDR, 300, 1000) == HAL_TIMEOUT) {} // wait for the EEPROM operation to be completed
+
+		printf("On position 0x%04X in memory data 0x%02X is located\n", addr, value);
+	}
+	else if (strcasecmp(token, "WRITE") == 0) // if the first parameter is WRITE,
+	{
+		static uint16_t addr = 0;
+		static uint8_t value = 0;
+		token = strtok(NULL, " "); // read address
+		addr = atoi(token);
+		token = strtok(NULL, " "); // read value to be written into the chosen address
+		value = atoi(token);
+
+		HAL_I2C_Mem_Write(&hi2c1, EEPROM_ADDR, addr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000); // bus name, device address, memory address, memory indexing method, where to save the read data, number of bytes to read, timeout
+		while (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_ADDR, 300, 1000) == HAL_TIMEOUT) {} // wait for the EEPROM operation to be completed
+
+		printf("OK\n");
+	}
+	else if (strcasecmp(token, "DUMP") == 0) // if the first parameter is DUMP, read the first 16 bytes of data from the EEPROM
+	{
+		static uint8_t value = 0;
+
+		for(uint8_t i = 0; i < read_num_of_bytes; i++) // increments through all of the desired addresses
+		{
+			HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, i, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000); // bus name, device address, memory address, memory indexing method, where to save the read data, number of bytes to read, timeout
+			while (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_ADDR, 300, 1000) == HAL_TIMEOUT) {} // wait for the EEPROM operation to be completed
+
+
+			if ((i + 1) % 8 == 0) printf("%02X \n", value); // separate rows after 8 bytes
+			else printf("%02X ", value);
+		}
+
+
+	}
+
 }
 
 
@@ -160,6 +210,7 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_UART_Receive_DMA(&huart2, uart_rx_buf, RX_BUFFER_LEN); // activate receive buffer
@@ -195,6 +246,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -223,6 +275,60 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x2000090E;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
